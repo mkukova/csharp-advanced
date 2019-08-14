139ae83b0cd23bd4a653cd8015d95e204ee1988f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-14 14:00:13 +0300

    Add matching-brackets

--- StacksAndQueues/4.MatchingBrackets/Program.cs
+++ StacksAndQueues/4.MatchingBrackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _4.MatchingBrackets
 {
@@ -6,7 +7,24 @@ namespace _4.MatchingBrackets
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine();
+			var stack = new Stack<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				char ch = input[i];
+
+				if (ch == '(')
+				{
+					stack.Push(i);
+				}
+				else if (ch == ')')
+				{
+					int startIndex = stack.Pop();
+					string contents = input.Substring(startIndex, i - startIndex + 1);
+					Console.WriteLine(contents);
+				}
+			}
 		}
 	}
 }
