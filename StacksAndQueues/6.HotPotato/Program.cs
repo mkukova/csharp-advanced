fed6191bdf99781c402d9b8d9ac9371124a763a2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-14 14:37:30 +0300

    Add hot-potato

--- StacksAndQueues/6.HotPotato/Program.cs
+++ StacksAndQueues/6.HotPotato/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _6.HotPotato
 {
@@ -6,7 +8,24 @@ namespace _6.HotPotato
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+			int count = int.Parse(Console.ReadLine());
+			Queue<string> children = new Queue<string>(input);
+
+			while (children.Count != 1)
+			{
+				for (int i = 1; i < count; i++)
+				{
+					string currentPlayer = children.Dequeue();
+					children.Enqueue(currentPlayer);
+				}
+
+				Console.WriteLine($"Removed {children.Dequeue()}");
+			}
+
+			Console.WriteLine($"Last is {children.Dequeue()}");
+
 		}
 	}
 }
