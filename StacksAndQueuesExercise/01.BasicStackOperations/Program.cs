650ac95ad3a4fddcdc60d78769ba33126f9cf477
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-16 11:06:03 +0300

    Add basic-stack-operations

--- StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
+++ StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01.BasicStackOperations
 {
@@ -6,7 +8,49 @@ namespace _01.BasicStackOperations
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] input = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int numberElementsToPush = input[0];
+			int numberElementsToPop = input[1];
+			int searchedElement = input[2];
+
+			int[] numbers = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			Stack<int> result = new Stack<int>();
+
+			for (int i = 0; i < numberElementsToPush; i++)
+			{
+				result.Push(numbers[i]);
+			}
+
+			for (int i = 0; i < numberElementsToPop; i++)
+			{
+				if (!result.Any())
+				{
+					break;
+				}
+
+				result.Pop();
+			}
+
+			if (result.Contains(searchedElement))
+			{
+				Console.WriteLine("true");
+			}
+			else if (result.Any())
+			{
+				Console.WriteLine(result.Min());
+			}
+			else
+			{
+				Console.WriteLine(0);
+			}
 		}
 	}
 }
