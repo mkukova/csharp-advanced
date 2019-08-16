1022bd6e4a5e7ecf29f6eba87f8be92651fd657f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-16 11:25:36 +0300

    Add maximum-and-mimimum-element

--- StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
+++ StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.MaximumAndMinimumElement
 {
@@ -6,7 +8,49 @@ namespace _03.MaximumAndMinimumElement
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int numberLines = int.Parse(Console.ReadLine());
+			var stack = new Stack<int>();
+
+			for (int i = 0; i < numberLines; i++)
+			{
+				int[] input = Console.ReadLine()
+					.Split(" ")
+					.Select(int.Parse)
+					.ToArray();
+
+				switch (input[0])
+				{
+					case 1:
+						stack.Push(input[1]);
+						break;
+					case 2:
+						if (stack.Any())
+						{
+							stack.Pop();
+						}
+
+						break;
+					case 3:
+						if (stack.Any())
+						{
+							Console.WriteLine(stack.Max());
+						}
+
+						break;
+					case 4:
+						if (stack.Any())
+						{
+							Console.WriteLine(stack.Min());
+						}
+
+						break;
+					default:
+						Console.WriteLine("Incorrect command!");
+						break;
+				}
+			}
+
+			Console.WriteLine(string.Join(", ", stack));
 		}
 	}
 }
