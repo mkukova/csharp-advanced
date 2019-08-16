522513bc0cf7b79eed03400c0f84c63050b35ebe
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-16 13:43:41 +0300

    Add truck-tour

--- StacksAndQueuesExercise/07.TruckTour/Program.cs
+++ StacksAndQueuesExercise/07.TruckTour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.TruckTour
 {
@@ -6,7 +8,47 @@ namespace _07.TruckTour
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int petrolPumpsCount = int.Parse(Console.ReadLine());
+			var petrolPumps = new Queue<int[]>();
+
+			for (int i = 0; i < petrolPumpsCount; i++)
+			{
+				int[] petrolPump = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+					.Select(int.Parse)
+					.ToArray();
+				petrolPumps.Enqueue(petrolPump);
+			}
+
+			int index = 0;
+
+
+			while (true)
+			{
+				int totalFuel = 0;
+
+				foreach (var petrolPump in petrolPumps)
+				{
+					int petrolAmount = petrolPump[0];
+					int distance = petrolPump[1];
+
+					totalFuel += petrolAmount - distance;
+
+					if (totalFuel < 0)
+					{
+						petrolPumps.Enqueue(petrolPumps.Dequeue());
+						index++;
+						break;
+					}
+				}
+
+				if (totalFuel >= 0)
+				{
+					break;
+				}
+			}
+
+			Console.WriteLine(index);
 		}
 	}
 }
