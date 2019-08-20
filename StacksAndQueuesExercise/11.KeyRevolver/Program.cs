226e3ec45e73b1b64add5f3949b347b9b18a730e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-20 15:17:33 +0300

    Add key-revolver

--- StacksAndQueuesExercise/11.KeyRevolver/Program.cs
+++ StacksAndQueuesExercise/11.KeyRevolver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11.KeyRevolver
 {
@@ -6,7 +8,60 @@ namespace _11.KeyRevolver
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int priceOfBullet = int.Parse(Console.ReadLine());
+			int gunBarrelSize = int.Parse(Console.ReadLine());
+			int[] bullets = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int[] locks = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int intelligenceValue = int.Parse(Console.ReadLine());
+			var lockQueue = new Queue<int>(locks);
+			var bulletsStack = new Stack<int>(bullets);
+			int totalBullets = bullets.Count();
+			int usedBullets = 0;
+
+			while (lockQueue.Any() && bulletsStack.Any())
+			{
+				int currentBullet = bulletsStack.Pop();
+				int currentLock = lockQueue.Peek();
+
+				if (currentBullet <= currentLock)
+				{
+					Console.WriteLine("Bang!");
+					lockQueue.Dequeue();
+				}
+				else
+				{
+					Console.WriteLine("Ping!");
+				}
+
+				usedBullets++;
+
+				if (usedBullets == gunBarrelSize && bulletsStack.Any())
+				{
+					Console.WriteLine("Reloading!");
+					usedBullets = 0;
+				}
+			}
+
+			if (!lockQueue.Any())
+			{
+				int leftBullets = bulletsStack.Count();
+				int totalUsedBullets = totalBullets - leftBullets;
+				int moneyEarned = intelligenceValue - (totalUsedBullets * priceOfBullet);
+				Console.WriteLine($"{leftBullets} bullets left. Earned ${moneyEarned}");
+			}
+			else if (!bulletsStack.Any())
+			{
+				int leftLocks = lockQueue.Count();
+				Console.WriteLine($"Couldn't get through. Locks left: {leftLocks}");
+			}
 		}
 	}
 }
