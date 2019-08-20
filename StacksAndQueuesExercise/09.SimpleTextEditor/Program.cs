b999e18d7ee1dbe7f0fc31d3625476bf68d6b05f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-20 13:52:05 +0300

    Add simple-text-editor

--- StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
+++ StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _09.SimpleTextEditor
 {
@@ -6,7 +8,43 @@ namespace _09.SimpleTextEditor
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var stackOfText = new Stack<string>();
+			StringBuilder text = new StringBuilder();
+			string countAsString = Console.ReadLine();
+			int count = int.Parse(countAsString);
+
+			for (int i = 0; i < count; i++)
+			{
+				string[] input = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+				string command = input[0];
+
+				if (command == "1")
+				{
+					stackOfText.Push(text.ToString());
+					string textToAppend = input[1];
+					text.Append(textToAppend);
+				}
+				else if (command == "2")
+				{
+					int index = int.Parse(input[1]);
+					stackOfText.Push(text.ToString());
+					int startIndex = text.Length - index;
+					text.Remove(startIndex, index);
+				}
+				else if (command == "3")
+				{
+					int index = int.Parse(input[1]);
+					Console.WriteLine(text[index - 1]);
+				}
+				else if (command == "4")
+				{
+					text.Clear();
+					string textToAppend = stackOfText.Pop();
+					text.Append(textToAppend);
+				}
+			}
 		}
 	}
 }
