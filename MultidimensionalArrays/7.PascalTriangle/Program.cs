7049fe938e28b3316f12e3f8d3d90d8e65bf0cb5
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-21 15:16:27 +0300

    Add pascal-triangle

--- MultidimensionalArrays/7.PascalTriangle/Program.cs
+++ MultidimensionalArrays/7.PascalTriangle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _7.PascalTriangle
 {
@@ -6,7 +7,33 @@ namespace _7.PascalTriangle
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int size = int.Parse(Console.ReadLine());
+			long[][] jaggedArr = new long[size][];
+			int cols = 1;
+
+			for (int i = 0; i < size; i++)
+			{
+				jaggedArr[i] = new long[cols];
+				jaggedArr[i][0] = 1;
+				jaggedArr[i][cols - 1] = 1;
+
+				if (cols > 2)
+				{
+					long[] previousRow = jaggedArr[i - 1];
+
+					for (int j = 1; j < cols - 1; j++)
+					{
+						jaggedArr[i][j] = previousRow[j] + previousRow[j - 1];
+					}
+				}
+
+				cols++;
+			}
+
+			foreach (long[] row in jaggedArr)
+			{
+				Console.WriteLine(string.Join(" ", row));
+			}
 		}
 	}
 }
