c9ed9dc6736964e176fc1281f6a97de9dc447567
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-21 12:37:43 +0300

    Add sum-matrix-elements

--- MultidimensionalArrays/1.SumMatrixElements/Program.cs
+++ MultidimensionalArrays/1.SumMatrixElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _1.SumMatrixElements
 {
@@ -6,7 +8,31 @@ namespace _1.SumMatrixElements
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] dimensions = Console.ReadLine()
+				.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			int[,] arr = new int[dimensions[0], dimensions[1]];
+			int sum = 0;
+
+			for (int i = 0; i < arr.GetLength(0); i++)
+			{
+				int[] tokens = Console.ReadLine()
+					.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+					.Select(int.Parse)
+					.ToArray();
+
+				for (int j = 0; j < arr.GetLength(1); j++)
+				{
+					arr[i, j] = tokens[j];
+					sum += arr[i, j];
+				}
+			}
+
+			Console.WriteLine(arr.GetLength(0));
+			Console.WriteLine(arr.GetLength(1));
+			Console.WriteLine(sum);
 		}
 	}
 }
