cd6f9ec64da7bcc0b50061d9dfe240f33f52147c
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-21 11:15:04 +0300

    Add cups-and-bottles

--- StacksAndQueuesExercise/12.CupsAndBottles/Program.cs
+++ StacksAndQueuesExercise/12.CupsAndBottles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _12.CupsAndBottles
 {
@@ -6,7 +8,53 @@ namespace _12.CupsAndBottles
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] cupsCapacity = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int[] bottles = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+
+			var bottlesStack = new Stack<int>(bottles);
+			var cupsQueue = new Queue<int>(cupsCapacity);
+			int wastedWater = 0;
+
+			while (cupsQueue.Any() && bottlesStack.Any())
+			{
+				int currentCup = cupsQueue.Peek();
+
+				while (currentCup != 0 && bottlesStack.Any())
+				{
+					int currentBottle = bottlesStack.Pop();
+
+					if (currentBottle - currentCup >= 0)
+					{
+						wastedWater += (currentBottle - currentCup);
+						currentCup = 0;
+						continue;
+					}
+
+					currentCup -= currentBottle;
+				}
+
+				cupsQueue.Dequeue();
+			}
+
+			if (bottlesStack.Any())
+			{
+				string remainingBottles = string.Join(" ", bottlesStack);
+				Console.WriteLine($"Bottles: {remainingBottles}");
+			}
+			else
+			{
+				string remainingCups = string.Join(" ", cupsQueue);
+				Console.WriteLine($"Cups: {remainingCups}");
+			}
+
+			Console.WriteLine($"Wasted litters of water: {wastedWater}");
 		}
 	}
 }
