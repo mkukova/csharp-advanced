b3a53058ce6040777386f5c083587081f8f6d1ac
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-22 13:51:37 +0300

    Add snake-moves

--- MultidimensionalArraysExercise/5.SnakeMoves/Program.cs
+++ MultidimensionalArraysExercise/5.SnakeMoves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5.SnakeMoves
 {
@@ -6,7 +7,39 @@ namespace _5.SnakeMoves
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] size = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			string snake = Console.ReadLine();
+			char[,] matrix = new char[size[0], size[1]];
+			int snakeLenght = snake.Length;
+			int counter = 0;
+
+			for (int row = 0; row < size[0]; row++)
+			{
+				for (int col = 0; col < size[1]; col++)
+				{
+					matrix[row, col] = snake[counter];
+					counter++;
+
+					if (counter >= snakeLenght)
+					{
+						counter = 0;
+					}
+				}
+			}
+
+			for (int i = 0; i < size[0]; i++)
+			{
+				for (int j = 0; j < size[1]; j++)
+				{
+					Console.Write(matrix[i, j]);
+				}
+
+				Console.WriteLine();
+			}
 		}
 	}
 }
