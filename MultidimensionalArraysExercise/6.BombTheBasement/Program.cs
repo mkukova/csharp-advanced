3606a72c4298609bdd4abef3b3c248c2e16419a3
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-22 15:19:03 +0300

    Add bomb-the-basement

--- MultidimensionalArraysExercise/6.BombTheBasement/Program.cs
+++ MultidimensionalArraysExercise/6.BombTheBasement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _6.BombTheBasement
 {
@@ -6,7 +7,65 @@ namespace _6.BombTheBasement
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] size = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			int[] bombParameters = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			int targetRow = bombParameters[0];
+			int targetCol = bombParameters[1];
+			int radius = bombParameters[2];
+
+			int[][] basement = new int[size[0]][];
+
+			for (int i = 0; i < basement.Length; i++)
+			{
+				basement[i] = new int[size[1]];
+			}
+
+			for (int row = 0; row < size[0]; row++)
+			{
+				for (int col = 0; col < size[1]; col++)
+				{
+					bool isInRadius = Math.Pow(row - targetRow, 2)
+						+ Math.Pow(col - targetCol, 2)
+						<= Math.Pow(radius, 2);
+
+					if (isInRadius)
+					{
+						basement[row][col] = 1;
+					}
+				}
+			}
+
+			for (int col = 0; col < basement[0].Length; col++)
+			{
+				int counter = 0;
+
+				for (int row = 0; row < basement.Length; row++)
+				{
+					if (basement[row][col] == 1)
+					{
+						counter++;
+						basement[row][col] = 0;
+					}
+				}
+
+				for (int row = 0; row < counter; row++)
+				{
+					basement[row][col] = 1;
+				}
+			}
+
+			foreach (int[] row in basement)
+			{
+				Console.WriteLine(string.Join("", row));
+			}
 		}
 	}
 }
