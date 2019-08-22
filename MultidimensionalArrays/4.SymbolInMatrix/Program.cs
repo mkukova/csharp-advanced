679c8d606dd467f3cdd3aa579f015798f93833b9
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-22 09:57:19 +0300

    Save code of symbol-in-matrix

--- MultidimensionalArrays/4.SymbolInMatrix/Program.cs
+++ MultidimensionalArrays/4.SymbolInMatrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4.SymbolInMatrix
 {
@@ -6,7 +7,46 @@ namespace _4.SymbolInMatrix
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int size = int.Parse(Console.ReadLine());
+			char[,] matrix = new char[size, size];
+
+			for (int i = 0; i < size; i++)
+			{
+				string tokens = Console.ReadLine();
+
+				for (int j = 0; j < size; j++)
+				{
+					matrix[i, j] = tokens[j];
+				}
+			}
+
+			char searchedSymbol = char.Parse(Console.ReadLine());
+			int row = -1;
+			int col = -1;
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (matrix[i, j] != searchedSymbol)
+					{
+						continue;
+					}
+
+					row = i;
+					col = j;
+					break;
+				}
+			}
+
+			if (row != -1 && col != -1)
+			{
+				Console.WriteLine($"({row}, {col})");
+			}
+			else
+			{
+				Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
+			}
 		}
 	}
 }
