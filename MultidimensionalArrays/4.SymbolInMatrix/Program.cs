4ccb73a83a77be2011d18c5f208cf57792ef8cdc
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-22 12:14:56 +0300

    Add symbol-in-matrix

--- MultidimensionalArrays/4.SymbolInMatrix/Program.cs
+++ MultidimensionalArrays/4.SymbolInMatrix/Program.cs
@@ -21,29 +21,27 @@ namespace _4.SymbolInMatrix
 			}
 
 			char searchedSymbol = char.Parse(Console.ReadLine());
-			int row = -1;
-			int col = -1;
+			bool isFound = false;
 
 			for (int i = 0; i < size; i++)
 			{
 				for (int j = 0; j < size; j++)
 				{
-					if (matrix[i, j] != searchedSymbol)
+					if (matrix[i, j] == searchedSymbol)
 					{
-						continue;
+						Console.WriteLine($"({i}, {j})");
+						isFound = true;
+						break;
 					}
+				}
 
-					row = i;
-					col = j;
+				if (isFound)
+				{
 					break;
 				}
 			}
 
-			if (row != -1 && col != -1)
-			{
-				Console.WriteLine($"({row}, {col})");
-			}
-			else
+			if (!isFound)
 			{
 				Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
 			}
