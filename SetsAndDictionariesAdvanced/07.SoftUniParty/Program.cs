bb22ead44e6512576a6dc75c79be6fe4408327bf
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-27 14:48:08 +0300

    Add softuni-party

--- SetsAndDictionariesAdvanced/07.SoftUniParty/Program.cs
+++ SetsAndDictionariesAdvanced/07.SoftUniParty/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07.SoftUniParty
 {
@@ -6,7 +7,59 @@ namespace _07.SoftUniParty
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var vipGuests = new HashSet<string>();
+			var regularGuests = new HashSet<string>();
+			string input = Console.ReadLine();
+
+			while (input != "PARTY")
+			{
+				if (char.IsDigit(input[0]))
+				{
+					vipGuests.Add(input);
+				}
+				else
+				{
+					regularGuests.Add(input);
+				}
+
+				input = Console.ReadLine();
+			}
+
+			while (input != "END")
+			{
+				if (vipGuests.Contains(input))
+				{
+					vipGuests.Remove(input);
+				}
+
+				if (regularGuests.Contains(input))
+				{
+					regularGuests.Remove(input);
+				}
+
+				input = Console.ReadLine();
+			}
+
+			int vipGuestCount = vipGuests.Count;
+			int regularGuestCount = regularGuests.Count;
+			int totalGuest = vipGuestCount + regularGuestCount;
+			Console.WriteLine(totalGuest);
+
+			if (vipGuestCount > 0)
+			{
+				foreach (string reservationNumber in vipGuests)
+				{
+					Console.WriteLine(reservationNumber);
+				}
+			}
+
+			if (regularGuestCount > 0)
+			{
+				foreach (string reservationNumber in regularGuests)
+				{
+					Console.WriteLine(reservationNumber);
+				}
+			}
 		}
 	}
 }
