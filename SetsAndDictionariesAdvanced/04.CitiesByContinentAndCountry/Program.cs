73ae0e4c5c7f06ab24641c41f0282ed27cdd50e4
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-27 14:02:39 +0300

    add cities-by-continent-and-country

--- SetsAndDictionariesAdvanced/04.CitiesByContinentAndCountry/Program.cs
+++ SetsAndDictionariesAdvanced/04.CitiesByContinentAndCountry/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.CitiesByContinentAndCountry
 {
@@ -6,7 +8,40 @@ namespace _04.CitiesByContinentAndCountry
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int count = int.Parse(Console.ReadLine());
+			var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+			for (int i = 0; i < count; i++)
+			{
+				string[] input = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+				string continent = input[0];
+				string country = input[1];
+				string city = input[2];
+
+				if (!continents.ContainsKey(continent))
+				{
+					continents[continent] = new Dictionary<string, List<string>>();
+				}
+
+				if (!continents[continent].ContainsKey(country))
+				{
+					continents[continent][country] = new List<string>();
+				}
+
+				continents[continent][country].Add(city);
+			}
+
+			foreach (var continent in continents)
+			{
+				Console.WriteLine($"{continent.Key}:");
+
+				foreach (var country in continent.Value)
+				{
+					string cities = string.Join(", ", country.Value);
+					Console.WriteLine($"{country.Key} -> {cities}");
+				}
+			}
 		}
 	}
 }
