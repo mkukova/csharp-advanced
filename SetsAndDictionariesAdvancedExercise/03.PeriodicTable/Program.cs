196c2df7ee488be640236b5b85a4d3bb72e5d6db
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-28 14:18:11 +0300

    Add periodic-table

--- SetsAndDictionariesAdvancedExercise/03.PeriodicTable/Program.cs
+++ SetsAndDictionariesAdvancedExercise/03.PeriodicTable/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.PeriodicTable
 {
@@ -6,7 +8,24 @@ namespace _03.PeriodicTable
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int inputLinesCount = int.Parse(Console.ReadLine());
+			var elements = new HashSet<string>();
+
+			for (int i = 0; i < inputLinesCount; i++)
+			{
+				string[] input = Console.ReadLine()
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+				for (int j = 0; j < input.Length; j++)
+				{
+					string currentElement = input[j];
+					elements.Add(currentElement);
+				}
+			}
+
+			elements = elements.OrderBy(x => x).ToHashSet();
+			string result = string.Join(" ", elements);
+			Console.WriteLine(result);
 		}
 	}
 }
