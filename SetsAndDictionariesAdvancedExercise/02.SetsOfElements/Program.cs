2acee6bdf7c906f3af2956e48e09e5873f878539
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-28 14:09:13 +0300

    Add sets-of-elements

--- SetsAndDictionariesAdvancedExercise/02.SetsOfElements/Program.cs
+++ SetsAndDictionariesAdvancedExercise/02.SetsOfElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _02.SetsOfElements
 {
@@ -6,7 +8,35 @@ namespace _02.SetsOfElements
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] setsLenght = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+			int firstSetLenght = setsLenght[0];
+			int secondSetLenght = setsLenght[1];
+			var firstSet = new HashSet<int>();
+			var secondSet = new HashSet<int>();
+
+			for (int i = 0; i < firstSetLenght; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+				firstSet.Add(number);
+			}
+
+			for (int i = 0; i < secondSetLenght; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+				secondSet.Add(number);
+			}
+
+			foreach (int number in firstSet)
+			{
+				if (secondSet.Contains(number))
+				{
+					Console.Write($"{number} ");
+				}
+			}
 		}
 	}
 }
