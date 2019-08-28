030deee525d48601d7531125e69834e5ce30da15
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-08-28 15:00:35 +0300

    Add wardrobe

--- SetsAndDictionariesAdvancedExercise/06.Wardrobe/Program.cs
+++ SetsAndDictionariesAdvancedExercise/06.Wardrobe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06.Wardrobe
 {
@@ -6,7 +7,64 @@ namespace _06.Wardrobe
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int countOfInputs = int.Parse(Console.ReadLine());
+			var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+			for (int i = 0; i < countOfInputs; i++)
+			{
+				string[] splittedInput = Console.ReadLine()
+					.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+				string colour = splittedInput[0];
+				string[] clothes = splittedInput[1]
+					.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+				if (!wardrobe.ContainsKey(colour))
+				{
+					wardrobe[colour] = new Dictionary<string, int>();
+				}
+
+				for (int j = 0; j < clothes.Length; j++)
+				{
+					string currentClothe = clothes[j];
+
+					if (!wardrobe[colour].ContainsKey(currentClothe))
+					{
+						wardrobe[colour][currentClothe] = 0;
+					}
+
+					wardrobe[colour][currentClothe]++;
+				}
+			}
+
+			string[] searchedItem = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+			string searchedColour = searchedItem[0];
+			string searchedClothe = searchedItem[1];
+			bool isSearchedColour = false;
+
+			foreach (var colour in wardrobe)
+			{
+				Console.WriteLine($"{colour.Key} clothes:");
+
+				if (colour.Key == searchedColour)
+				{
+					isSearchedColour = true;
+				}
+
+				foreach (var clothe in colour.Value)
+				{
+					if (isSearchedColour && clothe.Key == searchedClothe)
+					{
+						Console.WriteLine($"* {clothe.Key} - {clothe.Value} (found!)");
+					}
+					else
+					{
+						Console.WriteLine($"* {clothe.Key} - {clothe.Value}");
+					}
+				}
+			}
 		}
 	}
 }
