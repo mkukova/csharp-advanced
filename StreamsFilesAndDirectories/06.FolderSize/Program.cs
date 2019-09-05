d6d39f7eac1f9c984a34746e617aa6406beb4893
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-05 12:46:19 +0300

    Add folder-size

--- StreamsFilesAndDirectories/06.FolderSize/Program.cs
+++ StreamsFilesAndDirectories/06.FolderSize/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.IO;
 
 namespace _06.FolderSize
 {
@@ -6,7 +6,17 @@ namespace _06.FolderSize
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] files = Directory.GetFiles("TestFolder");
+			double sum = 0;
+
+			foreach (string file in files)
+			{
+				FileInfo fileInfo = new FileInfo(file);
+				sum += fileInfo.Length;
+			}
+
+			sum = sum / 1024 / 1024;
+			File.WriteAllText("output.txt", sum.ToString());
 		}
 	}
 }
