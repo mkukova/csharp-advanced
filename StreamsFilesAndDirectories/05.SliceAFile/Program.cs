8ed1bba9059b4fc82201fde70d40fa6947dc1e4c
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-05 12:37:02 +0300

    Add slice-a-file

--- StreamsFilesAndDirectories/05.SliceAFile/Program.cs
+++ StreamsFilesAndDirectories/05.SliceAFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _05.SliceAFile
 {
@@ -6,7 +7,25 @@ namespace _05.SliceAFile
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			FileStream inputFile = new FileStream(@"files\sliceMe.txt", FileMode.Open);
+
+			using (inputFile)
+			{
+				long size = inputFile.Length;
+				int partSize = (int)Math.Ceiling((double)size / 4);
+				var buffer = new byte[partSize];
+
+				for (int i = 1; i <= 4; i++)
+				{
+					FileStream outputFile = new FileStream($"files\\Part-{i}.txt", FileMode.Create);
+
+					using (outputFile)
+					{
+						int readedBytes = inputFile.Read(buffer, 0, partSize);
+						outputFile.Write(buffer, 0, readedBytes);
+					}
+				}
+			}
 		}
 	}
 }
