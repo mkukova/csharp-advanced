1d8680b5ebd4a2decea4505ed1ad53a17eb4b618
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-05 14:08:27 +0300

    Add merge-files

--- StreamsFilesAndDirectories/04.MergeFiles/Program.cs
+++ StreamsFilesAndDirectories/04.MergeFiles/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace _04.MergeFiles
 {
@@ -6,7 +8,25 @@ namespace _04.MergeFiles
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string path = "files";
+			string firstFileName = "FileOne.txt";
+			string secondFileName = "FileTwo.txt";
+			string firstPath = Path.Combine(path, firstFileName);
+			string secondPath = Path.Combine(path, secondFileName);
+
+			string[] firstInfo = File.ReadAllLines(firstPath);
+			string[] secondInfo = File.ReadAllLines(secondPath);
+
+			List<string> result = new List<string>();
+			result.AddRange(firstInfo);
+			result.AddRange(secondInfo);
+			string[] output = result
+				.OrderBy(x => x)
+				.ToArray();
+
+			string outputFileName = "Output.txt";
+			string outputPath = Path.Combine(path, outputFileName);
+			File.WriteAllLines(outputPath, output);
 		}
 	}
 }
