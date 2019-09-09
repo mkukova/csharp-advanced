49f313ffe9c1070633bac78cdca0833fba69452e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-09 14:43:27 +0300

    Add copy-binary-file

--- StreamsFilesAndDirectoriesExercise/04.CopyBinaryFile/Program.cs
+++ StreamsFilesAndDirectoriesExercise/04.CopyBinaryFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _04.CopyBinaryFile
 {
@@ -6,7 +7,30 @@ namespace _04.CopyBinaryFile
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string picPath = @"copyMe.png";
+			string picCopyPath = @"copyMe-copy.png";
+			FileStream streamReader = new FileStream(picPath, FileMode.Open);
+
+			using (streamReader)
+			{
+				FileStream writer = new FileStream(picCopyPath, FileMode.Create);
+
+				using (writer)
+				{
+					while (true)
+					{
+						var byteArray = new byte[4096];
+						int size = streamReader.Read(byteArray, 0, byteArray.Length);
+
+						if (size == 0)
+						{
+							break;
+						}
+
+						writer.Write(byteArray, 0, size);
+					}
+				}
+			}
 		}
 	}
 }
