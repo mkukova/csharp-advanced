48cfb54b3829457ecf34d876632f209ef3cab348
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-09 13:13:26 +0300

    Add word-count-exercise

--- StreamsFilesAndDirectoriesExercise/03.WordCount/Program.cs
+++ StreamsFilesAndDirectoriesExercise/03.WordCount/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.WordCount
 {
@@ -6,7 +9,49 @@ namespace _03.WordCount
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string textPath = @"text.txt";
+			string wordsPath = @"words.txt";
+			string[] textLines = File.ReadAllLines(textPath);
+			string[] words = File.ReadAllLines(wordsPath);
+			var wordsInfo = new Dictionary<string, int>();
+
+			foreach (string word in words)
+			{
+				string currentWordLowerCase = word.ToLower();
+
+				if (!wordsInfo.ContainsKey(word))
+				{
+					wordsInfo[word] = 0;
+				}
+			}
+
+			foreach (string currentLine in textLines)
+			{
+				string[] currentLineWords = currentLine
+					.ToLower()
+					.Split(new char[] { '.', ',', ';', '!', '?', '-', ' ', '\'', ':' });
+
+				foreach (string currentWord in currentLineWords)
+				{
+					if (wordsInfo.ContainsKey(currentWord))
+					{
+						wordsInfo[currentWord]++;
+					}
+				}
+			}
+
+			string actualResultPath = @"actualResult.txt";
+			string expectedResultPath = @"expectedResult.txt";
+
+			foreach ((string key, int value) in wordsInfo)
+			{
+				File.AppendAllText(actualResultPath, $"{key} - {value} {Environment.NewLine}");
+			}
+
+			foreach ((string key, int value) in wordsInfo.OrderByDescending(x => x.Value))
+			{
+				File.AppendAllText(expectedResultPath, $"{key} - {value} {Environment.NewLine}");
+			}
 		}
 	}
 }
