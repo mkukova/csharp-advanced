78154967530c5da2c07136c296d929cde96b05b4
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-10 14:00:45 +0300

    Add sort-even-numbers

--- FunctionalProgramming/01.SortEvenNumbers/Program.cs
+++ FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.SortEvenNumbers
 {
@@ -6,7 +7,14 @@ namespace _01.SortEvenNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] numbers = Console.ReadLine()
+				.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.Where(x => x % 2 == 0)
+				.OrderBy(x => x)
+				.ToArray();
+
+			Console.WriteLine(string.Join(", ", numbers));
 		}
 	}
 }
