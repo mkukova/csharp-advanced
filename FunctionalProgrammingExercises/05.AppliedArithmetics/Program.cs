95c959be22e66e07cdd8011f3ed7d7eb3855809f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 13:20:56 +0300

    Add applied-arithmetics

--- FunctionalProgrammingExercises/05.AppliedArithmetics/Program.cs
+++ FunctionalProgrammingExercises/05.AppliedArithmetics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05.AppliedArithmetics
 {
@@ -6,7 +7,45 @@ namespace _05.AppliedArithmetics
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] inputNumbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			Func<int, int> incrementByOne = x => x += 1;
+			Func<int, int> multiply = x => x *= 2;
+			Func<int, int> subtractByOne = x => x -= 1;
+			Action<int[]> print = numbers
+				=> Console.WriteLine(string.Join(" ", numbers));
+			string command = Console.ReadLine().ToLower();
+
+			while (command != "end")
+			{
+				if (command == "add")
+				{
+					inputNumbers = inputNumbers
+						.Select(incrementByOne)
+						.ToArray();
+				}
+				else if (command == "multiply")
+				{
+					inputNumbers = inputNumbers
+						.Select(multiply)
+						.ToArray();
+				}
+				else if (command == "subtract")
+				{
+					inputNumbers = inputNumbers
+						.Select(subtractByOne)
+						.ToArray();
+				}
+				else if (command == "print")
+				{
+					print(inputNumbers);
+				}
+
+				command = Console.ReadLine().ToLower();
+			}
 		}
 	}
 }
