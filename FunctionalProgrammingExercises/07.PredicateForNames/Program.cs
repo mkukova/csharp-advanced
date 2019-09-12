28b30bc7b67ad2294de081fc7dbaa404e94e0f36
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 15:38:32 +0300

    Add predicate-for-names

--- FunctionalProgrammingExercises/07.PredicateForNames/Program.cs
+++ FunctionalProgrammingExercises/07.PredicateForNames/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _07.PredicateForNames
 {
@@ -6,7 +8,16 @@ namespace _07.PredicateForNames
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int nameLength = int.Parse(Console.ReadLine());
+			List<string> names = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.ToList();
+
+			Predicate<string> isValidName = name
+				=> name.Length <= nameLength;
+
+			List<string> validNames = names.FindAll(isValidName);
+			Console.WriteLine(string.Join(Environment.NewLine, validNames));
 		}
 	}
 }
