f173ad5df3df31c4d38f50d3bbe0c789c62604a4
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 15:59:57 +0300

    Save current solution of predicate-party

--- FunctionalProgrammingExercises/10.PredicateParty!/Program.cs
+++ FunctionalProgrammingExercises/10.PredicateParty!/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10.PredicateParty_
 {
@@ -6,7 +8,29 @@ namespace _10.PredicateParty_
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<string> guests = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.ToList();
+
+			Func<string, string, bool> startsWithFilter = (name, param)
+				=> name.StartsWith(param);
+			Func<string, string, bool> endsWithFilter = (name, param)
+				=> name.EndsWith(param);
+			Func<string, int, bool> lengthFilter = (name, length)
+				=> name.Length == length;
+
+			string command = Console.ReadLine();
+
+			while (command != "Party!")
+			{
+				string[] splitttedCommand = command
+					.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+				string action = splitttedCommand[0];
+				string filter = splitttedCommand[1];
+				string param = splitttedCommand[2];
+
+				command = Console.ReadLine();
+			}
 		}
 	}
 }
