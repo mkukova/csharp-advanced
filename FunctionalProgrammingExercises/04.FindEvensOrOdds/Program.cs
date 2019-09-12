d0eab4c287314dbd58f3a3da2aadfe114260cb33
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 12:56:51 +0300

    Add find-evens-or-odds

--- FunctionalProgrammingExercises/04.FindEvensOrOdds/Program.cs
+++ FunctionalProgrammingExercises/04.FindEvensOrOdds/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.FindEvensOrOdds
 {
@@ -6,7 +8,41 @@ namespace _04.FindEvensOrOdds
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] bounds = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			int lowerBound = bounds[0];
+			int upperBound = bounds[1];
+			var numbers = new List<int>();
+			string numberType = Console.ReadLine().ToLower();
+
+			for (int i = lowerBound; i <= upperBound; i++)
+			{
+				numbers.Add(i);
+			}
+
+			Predicate<int> isEven = number => number % 2 == 0;
+			Predicate<int> isOdd = number => number % 2 != 0;
+			Action<List<int>> printNumbers = outputNumbers
+				=> Console.WriteLine(string.Join(" ", outputNumbers));
+
+			if (numberType == "odd")
+			{
+				numbers = numbers
+					.Where(x => isOdd(x))
+					.ToList();
+
+			}
+			else if (numberType == "even")
+			{
+				numbers = numbers
+					.Where(x => isEven(x))
+					.ToList();
+			}
+
+			printNumbers(numbers);
 		}
 	}
 }
