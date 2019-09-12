4f38c5e42962c12b583ff23d0764e87c79deb72d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 12:25:37 +0300

    Add action-print

--- FunctionalProgrammingExercises/01.ActionPrint/Program.cs
+++ FunctionalProgrammingExercises/01.ActionPrint/Program.cs
@@ -6,7 +6,13 @@ namespace _01.ActionPrint
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Action<string[]> printNames = names =>
+				Console.WriteLine(string.Join(Environment.NewLine, names));
+
+			string[] inputNames = Console.ReadLine()
+				.Split();
+
+			printNames(inputNames);
 		}
 	}
 }
