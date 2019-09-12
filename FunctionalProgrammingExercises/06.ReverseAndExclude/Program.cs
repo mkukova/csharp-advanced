9eb23b0998ee5de97a29d443e4cebda974849637
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 15:26:38 +0300

    Add reverse-and-exclude

--- FunctionalProgrammingExercises/06.ReverseAndExclude/Program.cs
+++ FunctionalProgrammingExercises/06.ReverseAndExclude/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.ReverseAndExclude
 {
@@ -6,7 +8,19 @@ namespace _06.ReverseAndExclude
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<int> numbers = Console.ReadLine()
+				.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.Reverse()
+				.ToList();
+
+			int divider = int.Parse(Console.ReadLine());
+
+			Predicate<int> divisible = num
+				=> num % divider == 0 ? true : false;
+
+			List<int> resultNumbers = numbers.FindAll(x => !divisible(x));
+			Console.WriteLine(string.Join(" ", resultNumbers));
 		}
 	}
 }
