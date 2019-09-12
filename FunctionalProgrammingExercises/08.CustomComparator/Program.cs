d8382f3fb61670384771f774723a2cc2983e8cbe
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-12 15:03:01 +0300

    Add custom-comparator

--- FunctionalProgrammingExercises/08.CustomComparator/Program.cs
+++ FunctionalProgrammingExercises/08.CustomComparator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08.CustomComparator
 {
@@ -6,7 +7,20 @@ namespace _08.CustomComparator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] numbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			Func<int, int, int> sortFunction = (x, y)
+				  => (x % 2 == 0 && y % 2 != 0) ? -1
+				  : (x % 2 != 0 && y % 2 == 0) ? 1
+				  : x > y ? 1
+				  : x < y ? -1
+				  : 0;
+
+			Array.Sort(numbers, (x, y) => sortFunction(x, y));
+			Console.WriteLine(string.Join(" ", numbers));
 		}
 	}
 }
