81fd93eacecc81415f23a81621a793c459ecad94
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-24 12:09:54 +0300

    Save code of car exercise

---
+++ DefiningClasses/1.Car/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.Car
+{
+	class Car
+	{
+		private int year;
+		private string make;
+		public string Make
+		{
+			get
+			{
+				return make;
+			}
+			set
+			{
+				this.make = value;
+			}
+		}
+		public string Model { get; set; }
+		public int Year
+		{
+			get
+			{
+				if (year < 5)
+				{
+					return year;
+				}
+
+				return year - 3;
+			}
+		}
+
+		public Car()
+		{
+			year = 1;
+		}
+
+		 public Car(int years)
+		{
+			this.year = years;
+		}
+
+		public int RealAge()
+		{
+			return year;
+		}
+
+		public void GetOld(int years)
+		{
+			year += years; 
+		}
+	}
+}
--- DefiningClasses/1.Car/Program.cs
+++ DefiningClasses/1.Car/Program.cs
@@ -6,7 +6,15 @@ namespace _1.Car
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Car myCar = new Car(13);
+			myCar.Make = "Renault";
+			myCar.Model = "Megan";
+
+			Console.WriteLine($"Make: {myCar.Make}\nModel: {myCar.Model}\nYear: {myCar.Year}");
+			Console.WriteLine($"Age: {myCar.RealAge()}");
+			myCar.GetOld(4);
+			Console.WriteLine($"Make: {myCar.Make}\nModel: {myCar.Model}\nYear: {myCar.Year}");
+			Console.WriteLine($"Age: {myCar.RealAge()}");
 		}
 	}
 }
