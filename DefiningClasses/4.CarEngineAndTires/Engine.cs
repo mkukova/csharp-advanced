c2e4a9bce7b4b73d9d48c1ae978851b5085b3c2f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-09-25 12:55:49 +0300

    Add car-engine-and-tires

---
+++ DefiningClasses/4.CarEngineAndTires/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4.CarEngineAndTires
+{
+	class Engine
+	{
+		private int horsePower;
+		private int cubicCapacity;
+		public int HorsePower { get; set; }
+		public double CubicCapacity { get; set; }
+
+		public Engine(int horsePower, double cubicCapacity)
+		{
+			this.HorsePower = horsePower;
+			this.CubicCapacity = cubicCapacity;
+		}
+	}
+}
--- DefiningClasses/4.CarEngineAndTires/Program.cs
+++ DefiningClasses/4.CarEngineAndTires/Program.cs
@@ -6,7 +6,17 @@ namespace _4.CarEngineAndTires
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Tire[] tires = new Tire[4]
+			{
+				new Tire(1, 2.5),
+				new Tire(1, 2.1),
+				new Tire(2, 0.5),
+				new Tire(2, 2.3)
+			};
+
+			Engine engine = new Engine(560, 6300);
+			Car myCar = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+			Console.WriteLine(myCar.WhoAmI());
 		}
 	}
 }
