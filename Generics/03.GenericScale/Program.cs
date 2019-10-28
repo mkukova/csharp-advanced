1b4f19a35a21e20320f8f3d159603a35ea1a85fb
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-10-28 13:09:32 +0200

    Add generic-scale

--- Generics/03.GenericScale/Program.cs
+++ Generics/03.GenericScale/Program.cs
@@ -6,7 +6,8 @@ namespace _03.GenericScale
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var scale = new EqualityScale<int>(5, 6);
+			Console.WriteLine(scale.AreEqual());
 		}
 	}
 }
